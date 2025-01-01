36e30310e3b170c4de1f2437a1d5c586d59ca851
wufake <wufake70@gmail.com>
2025-01-01 13:49:11 +0800

    周三 2025/01/01 13:49:11.45

--- src/java00_base/java000_cs.java
+++
@@ -1,9 +0,0 @@
-public class java000_cs {
-    public static void main(String[] args){
-        String a = "hello Java";
-        System.out.println(a);
-        a = "hello";
-        System.out.println(a);
-        
-    }
-}
